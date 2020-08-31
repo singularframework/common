2b52e01355651b119ccb81b763b78f58fb05b09c
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-31 01:41:26 -0700

    Changed transform and validate helpers input types; updated config type; added generic config type for merging the base server config with another type; renamed ServerConfig to BaseServerConfig.

--- src/common.ts
+++ src/common.ts
@@ -11,8 +11,10 @@ import {
   BodyTransformationDefinition,
   RequestTransformerFunction,
   AsyncRequestTransformerFunction,
-  CustomTransformationRule,
-  AggregationTarget
+  AggregationTarget,
+  PipeFunction,
+  AsyncPipeFunction,
+  ExecutablePipes
 } from './models';
 
 export * from './models';
@@ -44,9 +46,9 @@ export namespace validate {
   /** Creates validation rule for request query parameters. */
   export function queries(validator: ValidationDefinition): ValidationRule { return { target: AggregationTarget.Queries, validator }; }
   /** Creates validation rule for request body. */
-  export function body(validator: BodyValidationDefinition): ValidationRule { return { target: AggregationTarget.Body, validator }; }
+  export function body(validator: BodyValidationDefinition|ValidatorFunction|AsyncValidatorFunction|ExecutableValidators): ValidationRule { return { target: AggregationTarget.Body, validator }; }
   /** Creates custom validation rule for request. */
-  export function custom(validator: AsyncValidatorFunction|ValidatorFunction|ExecutableValidators): ValidationRule { return { target: AggregationTarget.Custom, validator } };
+  export function custom(validator: AsyncValidatorFunction|ValidatorFunction): ValidationRule { return { target: AggregationTarget.Custom, validator } };
 
 }
 
@@ -57,8 +59,8 @@ export namespace transform {
   /** Creates transformation rule for request query parameters. */
   export function queries(transformer: TransformationDefinition|'origin'): TransformationRule { return { target: AggregationTarget.Queries, transformer }; }
   /** Creates transformation rule for request body. */
-  export function body(transformer: BodyTransformationDefinition|'origin'): TransformationRule { return { target: AggregationTarget.Body, transformer }; }
+  export function body(transformer: BodyTransformationDefinition|PipeFunction|AsyncPipeFunction|ExecutablePipes|'origin'): TransformationRule { return { target: AggregationTarget.Body, transformer }; }
   /** Creates custom transformation rule for request. */
-  export function custom(transformer: RequestTransformerFunction|AsyncRequestTransformerFunction): CustomTransformationRule { return { target: AggregationTarget.Custom, transformer } };
+  export function custom(transformer: RequestTransformerFunction|AsyncRequestTransformerFunction): TransformationRule { return { target: AggregationTarget.Custom, transformer } };
 
 }
--- src/models.ts
+++ src/models.ts
@@ -2,14 +2,16 @@ import { Request as OriginalRequest } from 'express';
 import { CorsOptions } from 'cors';
 
 export { Response, NextFunction } from 'express';
-export type PipeFunction = (value: any, rawValues?: any) => Omit<void, any>;
-export type AsyncPipeFunction = (value: any, rawValues?: any) => Promise<Omit<void, any>>;
+export type PipeFunction = (value: any, rawValues?: any) => Exclude<Exclude<any, void>, Promise<any>>;
+export type AsyncPipeFunction = (value: any, rawValues?: any) => Promise<Exclude<any, void>>;
 export type RequestTransformerFunction = (req: Request) => void;
 export type AsyncRequestTransformerFunction = (req: Request) => Promise<void>;
 export type ValidatorFunction = (value: any, rawValues?: any) => boolean|Error;
 export type AsyncValidatorFunction = (value: any, rawValues?: any) => Promise<boolean|Error>;
 
-export interface ServerConfig {
+export type ServerConfig<T=BaseServerConfig> = BaseServerConfig & T;
+
+export interface BaseServerConfig {
 
   port?: number;
   predictive404?: boolean;
@@ -24,9 +26,11 @@ export interface ServerConfig {
   excludeQueryParamsInLogs?: string[];
   excludeHeadersInLogs?: string[];
   logRequestHeaders?: boolean;
+  logResponseMessages?: boolean;
   fileUploadLimit?: string;
   sessionManagement?: boolean;
   cookieSecret?: string;
+  enableCors?: boolean;
   [key: string]: any;
 
 }
@@ -143,14 +147,7 @@ export interface ValidationRule {
 export interface TransformationRule {
 
   target: AggregationTarget;
-  transformer: BodyTransformationDefinition|TransformationDefinition|PipeFunction|AsyncPipeFunction|ExecutablePipes|'origin';
-
-}
-
-export interface CustomTransformationRule {
-
-  target: AggregationTarget.Custom;
-  transformer: AsyncRequestTransformerFunction|RequestTransformerFunction;
+  transformer: BodyTransformationDefinition|TransformationDefinition|PipeFunction|AsyncPipeFunction|RequestTransformerFunction|AsyncRequestTransformerFunction|ExecutablePipes|'origin';
 
 }
 
